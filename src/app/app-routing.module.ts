239b2bef6ea19292980b2cc68ef46ce31ca9f71f
abdelaleemadel <ahmedadel2912@gmail.com>
2023-12-16 20:28:06 +0200

    Add projects, and skills in home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { FrameworksComponent } from './components/frameworks/frameworks.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  {
+    path: 'home', component: HomeComponent, children: [
+      { path: '', redirectTo: 'languages', pathMatch: 'full' },
+      { path: 'languages', component: LanguagesComponent },
+      { path: 'frameworks', component: FrameworksComponent },
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { FrameworksComponent } from './components/frameworks/frameworks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProjectsComponent,
+    LanguagesComponent,
+    FrameworksComponent
   ],
   imports: [
     BrowserModule,
